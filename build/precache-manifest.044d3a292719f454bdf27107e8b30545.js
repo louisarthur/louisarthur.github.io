41bc5a20b70b6543192113bd895900ec0b7260f6
Louis Arthur <louisartur.53@gmail.com>
2019-11-13 18:00:44 -0300

    adding firebase to application

---
+++ build/precache-manifest.044d3a292719f454bdf27107e8b30545.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c37b2a983f81a0169d3632eb7cd2980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3c5ae4e8d9286e3f267",
+    "url": "/static/js/2.91b254a6.chunk.js"
+  },
+  {
+    "revision": "759a711264deccf13b5f",
+    "url": "/static/js/main.54bdd14a.chunk.js"
+  },
+  {
+    "revision": "ab07d74e1bdcb986af0d",
+    "url": "/static/js/runtime-main.f28ad287.js"
+  }
+]);
